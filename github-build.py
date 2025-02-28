3d26d57ccbb56f81ae598488a374317d397ba39e
Stefan-Olt <stefan-oltmanns@gmx.net>
2025-03-01 00:41:57 +0100

    windows build fix

--- github-build.py
+++ github-build.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import platform as pf
 import sys
 import subprocess
+
+def exec_command(cmd: list) -> int:
+    print(" ".join(cmd))
+    if pf.uname().system.startswith('Windows'):
+        return subprocess.call(cmd, shell=True, universal_newlines=True)
+    else:
+        return subprocess.call(cmd, shell=False, universal_newlines=True)
 
 git_output = subprocess.check_output(['git', 'diff', '--name-only', 'HEAD', 'HEAD~1'])
 plugin_list = git_output.decode("utf-8").strip().split("\n")
@@ -15,15 +23,15 @@ for p in plugin_list:
     p = os.path.splitext(os.path.basename(p))[0]
     print("Will build plugin "+p)
     if os.path.isdir('workspace'):
-        os.system("cp -rp workspace .workspace-backup")
-    ret = os.system("./vsp-build.py "+p)
+        exec_command(["cp", "-rp", "workspace", ".workspace-backup"])
+    ret = exec_command(["./vsp-build.py", p])
     if ret == 0:
         try:
             with open('output/TAG', 'r') as file:
                 tag = file.read().strip()
-            if os.system("git tag "+tag) != 0:
+            if exec_command(["git", "tag", tag]) != 0:
                 raise Exception("Could not create git tag") 
-            if os.system("git push origin tag "+tag) != 0:
+            if exec_command(["git", "push", "origin", "tag", tag]) != 0:
                 raise Exception("Could not push git tag") 
             output_file = None
             for f in os.listdir("output"):
@@ -32,9 +40,9 @@ for p in plugin_list:
                     break
             if output_file == None:
                 raise Exception("Could not find output file") 
-            if os.system("gh release create "+tag+" --repo "+sys.argv[1]+" --title "+output_file+' --notes "Automatic build of '+p+'" --verify-tag') != 0:
+            if exec_command(["gh", "release", "create", tag, "--repo", sys.argv[1], "--title", output_file, "--notes", '"Automatic build of '+p+'"', "--verify-tag"]) != 0:
                 raise Exception("Could not create release") 
-            if os.system("gh release upload "+tag+" ./output/"+output_file+" --repo "+sys.argv[1]) != 0:
+            if exec_command(["gh", "release", "upload", tag, "./output/"+output_file, "--repo", sys.argv[1]]) != 0:
                 raise Exception("Could upload file to release")
             success_list.append(p)
         except Exception as err:
@@ -42,12 +50,12 @@ for p in plugin_list:
             failed_list.append(p + ": "+str(err))
     else:
         failed_list.append(p + ": "+str(ret))
-    os.system("rm -rf build")
-    os.system("rm -rf workspace")
-    os.system("rm -rf output")
-    os.system("rm -rf test")
+    exec_command(["rm", "-rf", "build"])
+    exec_command(["rm", "-rf", "workspace"])
+    exec_command(["rm", "-rf", "output"])
+    exec_command(["rm", "-rf", "test"])
     if os.path.isdir('.workspace-backup'):
-        os.system("mv .workspace-backup workspace")
+        exec_command(["mv", ".workspace-backup", "workspace"])
 
 with open('build.log', 'w') as file:
     file.write("Plugins build successfully:\n"+"\n".join(success_list)+"\n\n\nPlugin build failed:\n"+"\n".join(failed_list))
